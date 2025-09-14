60b642dea06e08ec6a0b89130600fad989bf3d38
Kirsy <74831242+phuoc-stack@users.noreply.github.com>
2025-09-14 11:11:47 +1000

    Time: 15 ms (47.6%), Space: 58.5 MB (39.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,11 +3,9 @@ class Solution {
         Map<Integer, Integer> records = new HashMap<>();
         for (int i=0; i<=nums.length-1; i++){
             if (records.get(nums[i]) != null){
-                System.out.println("duplicated value of" + nums[i]);
                 return true;
             }
             else {
-                System.out.println("no duplicated value of" + nums[i]);
                 records.put(nums[i], 1);
             }
         }
