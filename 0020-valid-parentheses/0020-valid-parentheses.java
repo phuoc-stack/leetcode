2fbf06b9bfb08ab20b002cdf8e992bead4dddd0e
Kirsy <74831242+phuoc-stack@users.noreply.github.com>
2025-09-27 10:21:56 +1000

    Time: 6 ms (5.14%), Space: 41.9 MB (62.91%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != map.get(c)) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
